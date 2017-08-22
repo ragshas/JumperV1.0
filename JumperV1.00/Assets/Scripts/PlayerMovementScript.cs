efa4c7faa3e0416f791487f076d74d15f550153d
Raghab Shrestha <ragshas@gmail.com>
2017-08-22 11:09:53 +0545

    Horizontal and vertical movement added

---
+++ JumperV1.00/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    Rigidbody rb;
+    public Boundary b;
+
+    public float speed = 10.0f;
+    public float jumpheight = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void Update()
+    {
+
+    }
+    void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3(Mathf.Clamp(rb.position.x, b.xMin, b.xMax), 0.0f,
+                                  Mathf.Clamp(rb.position.z,b.zMin,b.zMax));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(0.0f, 10 * jumpheight * Time.deltaTime, 0.0f);
+        }
+    }
+}
